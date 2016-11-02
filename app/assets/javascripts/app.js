e03adb2d541ddc1cc4b5a91366bf75ed63270d4d
stnagy <stnagy@gmail.com>
2016-11-02 14:19:26 +0000

    angular fix

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,7 +6,7 @@ angular.module('HOVDirection', [
   'HOVDirection.Home' // homepage
 ])
 
-  .config(function($mdThemingProvider) {
+  .config(['$mdThemingProvider', function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
     .primaryPalette('light-blue', {
     'default': '700', // by default use shade 400 from the pink palette for primary intentions
@@ -20,7 +20,7 @@ angular.module('HOVDirection', [
     'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
     'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
   });
-})
+}])
 
   .config([
   '$stateProvider',
