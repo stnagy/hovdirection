675c40c44f0aea446863103af1828497d4ccf499
stnagy <stnagy@gmail.com>
2016-12-06 11:58:07 +0000

    hotfix for rack attack

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -3,8 +3,7 @@ class Rack::Attack
     # Block China
     Rack::Attack.blocklist('block china') do |req|
         # Requests are blocked if the return value is truthy
-        return true if req.ip.match(/\A180\.76\./) 
-        return false
+        req.ip.match(/\A180\.76\./) 
     end
   
 end
